d81bc8d68a8c2ae012170a2c5a509055ea661249
mukango <43271452+mukango@users.noreply.github.com>
2018-10-05 10:39:39 +0200

    Update FAQPage.js
    First FAQ section updated.

--- src/containers/FAQPage/FAQPage.js
+++ src/containers/FAQPage/FAQPage.js
@@ -50,22 +50,51 @@ General questions
              <div className={css.contentWrapper}>
 
              <div className={css.contentSide}>
-             <p>Where does my money go?</p>
+             <p>How does Goodwings Experiences work?</p>
            </div>
 
            <div className={css.contentMain}>
-             <h2>
-It helps the world!
-
-             </h2>
 
              <p>
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum magna ullamcorper, tincidunt nulla sed, ultrices massa. Curabitur blandit vulputate dictum. Nullam et orci in eros iaculis sollicitudin. Proin et lorem urna. Cras a odio est. Nulla volutpat, nulla quis condimentum egestas, ipsum lectus hendrerit quam, vitae ornare turpis est sit amet tortor. Pellentesque porttitor, velit in scelerisque consectetur, ex ligula fringilla tellus, nec interdum felis eros quis massa. Nullam at neque interdum, eleifend sapien in, malesuada urna. Sed quis dui consectetur, semper velit vitae, maximus est.
-
+             Nonprofit organisations add their social impact activities to the marketplace. Travel agencies/tour operators – as well as other companies looking for authentic experiences – can inquire about the posted activities and book them directly through the marketplace by making a secure payment via bank account or an international creditcard.
 
              </p>
             </div>
           </div>
+  
+   <div className={css.contentWrapper}>
+            <div className={css.contentSide}>
+              <p>Who receives the money?</p>
+            </div>
+
+            <div className={css.contentMain}>
+             
+
+              <p>
+             The price of any given experience on the marketplace consists of the amount paid to nonprofit organisation (the experience host) and a 20% transaction fee. There are no other hidden fees. Example, if an experience costs USD 500, the nonprofit organisation hosting the experience receives this amount. On top of that, we add a 20% transaction fee paid by the customer. This fee makes the marketplace possible. In this example, the total price paid by the customer will be USD 600.  Goodwings handle everything relating to the secure payment.
+
+              </p>
+             </div>
+             </div>
+             <div className={css.contentWrapper}>
+
+             <div className={css.contentSide}>
+             <p>What is Goodwings?</p>
+           </div>
+
+           <div className={css.contentMain}>
+
+             <p>
+             Goodwings is a Danish company. We run www.goodwings.com – a hotel booking platform for corporate travel where every booking results in a donation to a nonprofit. 
+
+We don't spend any money on advertising like the other large hotel booking sites. Instead, we have a global network of nonprofit partners and they act as our marketing channel, spreading the good news about Goodwings. In return, we donate part of our commission to them. That's how we're turning the travel industry into a force for positive and sustainable change. One hotel at a time.
+
+Read more about us. Link: https://www.goodwings.com/en-us/about-us/vision-mission
+
+             </p>
+            </div>
+          </div>
+  
         </LayoutWrapperMain>
 
         <LayoutWrapperFooter>
