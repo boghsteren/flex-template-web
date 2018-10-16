16b4d43417e467aedf0d33e74771f63b064c7d35
Jakob Sorensen <boghsteren@gmail.com>
2018-10-16 11:51:32 +0200

    Moved capacity and added group size label to listing page

--- src/containers/ListingPage/SectionDescription.js
+++ src/containers/ListingPage/SectionDescription.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
 import { richText } from "../../util/richText";
+import config from "../../config";
 
 import css from "./ListingPage.css";
 
@@ -8,14 +9,11 @@ const MIN_LENGTH_FOR_LONG_WORDS_IN_DESCRIPTION = 20;
 
 const SectionDescription = props => {
   const { description, group_size, included, duration } = props;
+  const group_size_label = config.custom.group_size_brackets.find(
+    entry => entry.key === group_size
+  ).label;
   return (
     <div>
-      <div className={css.sectionDescription}>
-        <h2 className={css.descriptionTitle}>
-          <FormattedMessage id="ListingPage.group_sizeHeader" />
-        </h2>
-        <p className={css.description}>{group_size}</p>
-      </div>
       <div className={css.sectionDescription}>
         <h2 className={css.descriptionTitle}>
           <FormattedMessage id="ListingPage.descriptionTitle" />
@@ -43,6 +41,12 @@ const SectionDescription = props => {
           <p className={css.description}>{duration}</p>
         </div>
       )}
+      <div className={css.sectionDescription}>
+        <h2 className={css.descriptionTitle}>
+          <FormattedMessage id="ListingPage.group_sizeHeader" />
+        </h2>
+        <p className={css.description}>{group_size_label}</p>
+      </div>
     </div>
   );
 };
