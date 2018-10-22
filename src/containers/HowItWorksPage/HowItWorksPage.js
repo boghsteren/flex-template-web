e67ee1da8ba722a47be417a99ef057550ecf3ae8
mukango <43271452+mukango@users.noreply.github.com>
2018-10-22 16:33:29 +0200

    Update title

--- src/containers/HowItWorksPage/HowItWorksPage.js
+++ src/containers/HowItWorksPage/HowItWorksPage.js
@@ -29,7 +29,7 @@ const HowItWorksPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>How it works.</h1>
+          <h1 className={css.pageTitle}>How it works</h1>
           <h2>1. Search for an experience</h2>
 
           <p>
