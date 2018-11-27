adac85224b88b9da6fec918ebbb480ddd3ee981f
mukango <43271452+mukango@users.noreply.github.com>
2018-11-27 11:19:17 +0100

    Update PrivacyPolicy.js

--- src/components/PrivacyPolicy/PrivacyPolicy.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -11,54 +11,181 @@ const PrivacyPolicy = props => {
   // prettier-ignore
   return (
     <div className={classes}>
-      <p className={css.lastUpdated}>Last updated: September 28, 2018</p>
+      <p className={css.lastUpdated}>Last updated: November 27, 2018</p>
 
 
-<h2>      Registrar</h2>
-
-<h3>Goodwings Experiences service: </h3>
-<p>Sharetribe Ltd</p>
-<p>www.sharetribe.com</p>
-<p>info (at) sharetribe.com</p>
-<h3>Contact person:</h3> 
-<p>Antti Virolainen</p>
-<p>Kirjurinkuja 3 F 46 02600 Espoo</p>
-<p>+358-50-5311793</p>
+<h2>1.	 INTRODUCTION</h2>
+<p>1.1	Goodwings ApS, (hereafter “Goodwings Experiences”, ”we”, ”us” or ”our”) processes data about current and potential users, partners and suppliers as well as visitors to our website(s) or users of our associated services.</p>
+<p>1.2	We process your personal data in accordance with this privacy and cookie policy (hereafter "Privacy Policy").</p>
+<p>1.3	This Privacy Policy applies from November 27, 2018. Goodwings Experiences reserves the right to amend this Privacy Policy at any time with an appropriate notice to the users and users.</p>
+<h2>2.	DATA CONTROLLER</h2>
+<p>2.1	Data controller.</p>
+<p>Goodwings Experiences is the data controller and we ensure that personal data is processed in accordance with applicable law.</p>
+<p>2.2	We take the protection of your personal data seriously.</p>
+<p>We collect and process personal data whilst operating the website (www.gwexperiences.com) and have therefore adopted this Privacy Policy. The Privacy Policy informs you of how we process the personal data we collect and use when you use our services</p>
+<p> 2.3	We ensure a fair and transparent data processing.</p>
+<p>When we ask you to make your personal data available to us, we inform you of the personal data we are processing about you and the purpose hereof. You will receive this information when your personal data is being collected.</p>
+<p>2.4	Contact information.</p>
+<p>If you would like to get in touch with us, our contact information is:</p>
+<p>Goodwings Experiences<br>
+  Gammel Kongevej 5C, 1st floor<br>
+  1610 Copenhagen V<br>
+  Denmark<br>
+  CVR-no.: 37036749<br>
+  Phone: +45 61 68 93 62<br>
+E-mail: <a href="mailto:hello@gwexperiences.com">hello@gwexperiences.com</a></p>
+<h2>3.	PROCESSING OF PERSONAL DATA</h2>
+<p>3.1	Use of our website without signing up for services.</p>
+<p> It is possible to use our website without us collecting your personal data as described in the following. However, please be aware that our website uses cookies, and deactivating cookies can potentially reduce our website’s functionality. You can read more about cookies in section 6 below.</p>
+<p>3.2	We collect and keep your personal data for specific purposes.</p>
+<p>We collect and keep your personal data in accordance with specific purposes. Processing occurs when we need to:</p>
+<ul>
+  <li>Administer your relation to us;</li>
+  <li>Improve our services;</li>
+  <li>Adjust our communication and marketing to you;</li>
+  <li>Keep statistics. </li>
+</ul>
+<p>In the following you can read more about the individual services. We always strive to comply with the general principles described for each individual service.</p>
+<p>3.2.1	Users’ personal data.</p>
+<p>As data controller Goodwings Experiences collects and processes users’ personal data to: </p>
+<ul>
+  <li>Be able to provide the advice and services to you that Goodwings Experiences takes on to deliver to you as a user, </li>
+  <li>Live up to Goodwings Experiences’ contractual and legal obligations and or</li>
+  <li>Adhere to and live up to general legal obligations. </li>
+</ul>
+<p>The legal basis for Goodwings Experiences’ processing of users’ personal data for the purposes mentioned above will in most cases be, that the processing is necessary in order for us to: </p>
+<ul>
+  <li>fulfill the agreement with the user, including Goodwings Experiences’ Terms of Service, or other contract that you are party to, or to engage measures ahead of the entering into the agreement, </li>
+  <li>adhere to a legal obligation that Goodwings Experiences is subject to by law, as well as, <br />
+    to assess, argue or defend a legal claim, or when the courts act in their ability as a court. </li>
+</ul>
+<p>Most personal data will be collected from the users themselves. Such personal data will usually be: </p>
+<ul>
+  <li>Personal details: Name, email address, phone number, street address; </li>
+  <li> Account details: username, password (stored in encrypted format); </li>
+  <li>The description text that the user may write about him/herself; </li>
+  <li> The offers and requests the user has posted to the service; </li>
+  <li> The given and received feedback and badges; </li>
+  <li> Statistical data about service usage, e.g. number times the user has logged in. </li>
+</ul>
+<P>Details can be used for communication between service providers and users and for direct communication between users. Some personal details are visible on the profile page of the user, but those details are voluntary (except name).</P>
+<P>Goodwings Experiences usually collects personal from the user upon registration to the service or when using it later.</P>
+<P>The information may be handed over for research purposes as described in the Terms of Service that the user accepts before starting to use the service. The researchers may not publish any research results so that identifying information would be revealed or that any specific user could be detected.</P>
+<p>3.2.2	When you sign-up for services on our website or contact us.</p>
+<p>We use your personal data when we provide our services to you and make communication possible, while improving and ensuring the quality of the service, as well as in our contact with you.</p>
+<p>The personal data that may be stored in the register mainly includes:</p>
+<ul>
+  <li>Personal contact information: Name, email address, phone number, street address;</li>
+  <li>Account information: username, password (stored in encrypted format);
+  </li>
+</ul>
+<p>Goodwings Experiences does not collect personal data about our users or users from any third parties.</p>
+<p>3.2.3	Inquiries to us.</p>
+<p>If you contact us, we will process the information necessary in order for us to handle inquires, complaints, etc. 
+  The legal basis for this is our legitimate interests in user service, as well as establishing, exercising and defending against legal claims, as we will assess each case individually to determine whether our legitimate interests in initiating a professional relationship exceeds your interests or fundamental rights and freedoms, which requires protection of personal data. 
+</p>
+<p>3.2.4	Suppliers and partners.</p>
+<p>We may collect personal data relating to suppliers and partners, including mainly contact information for the contact person etc. The information will mainly be name and contact information.
+  The legal basis for this will be our legitimate interests in fulfilling the contacts that either have been or will be entered into, conducting our business, as well as establishing, exercising and defending against legal claims, as we will assess each case individually to determine whether our legitimate interests in registering the personal data exceeds the data subject’s interests or fundamental rights and freedoms, which requires protection of personal data.  
+  
+</p>
+<h2>4.	PROCESSING OF PERSONAL DATA
+</h2>
+<p>4.1	Data processors.</p>
+<p>In order to deliver our services to you, it is necessary for us to disclose relevant personal data on you to our business partners such as IT-hosting companies.
+</p>
+<p>Sharetribe Ltd
+</p>
+<p>www.sharetribe.com
+  info (at) sharetribe.com
+  
+</p>
+<p>Contact person:
+</p>
+<p>Antti Virolainen
+</p>
+<p>Kirjurinkuja 3 F 46 02600 Espoo
+</p>
+<p>+358-50-5311793
+</p>
 <p>antti.virolainen (at) sharetribe.com
 </p>
-<h2>Name of the register</h2>
-<p>User register of the Goodwings Experiences service
+<p>We only use data processors providing sufficient guarantees to implement appropriate technical and organisational measures in such a manner that processing will meet the requirements of the relevant data protection laws, including ensuring the protection of the rights of the data subject.</p>
+<p>4.2	Transfer.</p>
+<p>We may transfer personal data to third parties for research purposes as described in the Terms of Service.
+Information may be stored to a server that may be located inside or outside of the EU and the European Economic Area.</p>
+<p>4.3	We only process relevant personal data.</p>
+<p>We only collect, process and store personal data which is relevant in relation to fulfill the purposes, as defined above. The purpose is decisive for which of your personal data is relevant to us. The same applies to the extent of the personal data that we use. For example we do not use other personal data than what is necessary to fulfill the relevant purpose. 
 </p>
-<h2>Use of personal details (purpose of register)</h2>
-<p>Personal details are collected to make communication and use of service possible. Details can be used for communication between service providers and users and also for direct communication between users. Some personal details are visible on the profile page of the user, but those details are voluntary (except name).
+<p>4.4	We only process necessary personal data.</p>
+<p>We only collect, process and store personal data which is necessary in order to fulfill a specific purpose.</p>
+<p>4.5	We check and update your personal data.</p>
+<p>We ensure that the personal data which we process about you is not incorrect or misleading. We also make sure to update your personal data continuously. 
 </p>
-<p>The handling of personal details is not outsourced, but the register data is stored on a server that is rented from a third party company.
+<p>4.6	We delete your personal data when it is no longer necessary to keep.</p>
+<p>We will delete your personal data when it is no longer required in relation to the purpose that was the reason for our collection, processing and storage of your personal data. 
+  We will store personal data that we are obliged to keep in accordance with the bookkeeping legislation e.g. 
 </p>
-<h2>Information content of the register</h2>
-<p>
-The following information may be stored in the register:</p>
-<ul>
-<li>- Personal details: Name, email address, phone number, street address</li>
-<li>- Account details: username, password (stored in encrypted format)</li>
-<li>- The description text that the user may write about him/herself</li>
-<li>- The offers and requests the user has posted to the service</li>
-<li>- The given and received feedback and badges</li>
-<li>- Statistical data about service usage, e.g. number times the user has logged in</li>
-</ul>
-
-<h2>Regular sources of information</h2><p>
-Personal details are given by the user on registration to the service or when using it later.
+<p>4.7	We obtain your consent before processing your personal data.</p>
+<p>We will obtain your consent before processing your personal data for the purposes described above, unless there is another legal basis for the collection. We will notify you of such legal basis and of our legitimate interests for processing your personal data.
+Your consent is voluntary, and you can at any time withdraw it by contacting us. Use the contact information stated in section 2.4 if you wish to withdraw your consent or want further information. The withdrawal of you consent will not affect the lawfulness of processing based on consent before its withdrawal.</p>
+<p>4.8	We do not disclose your personal data without your consent.</p>
+<p>We will collect your consent and give you information concerning what your personal data will be used for before and if we disclose your personal data to business partners and others, e.g. for marketing purposes. You can, at any time, object to this kind of disclosure. 
+  
+We do not collect your consent if we are legally obliged to disclose your personal data, e.g. as part of a regulatory filing with an authority.</p>
+<h2>5.	SECURITY
+</h2>
+<p>5.1	We protect your personal data.</p>
+<p>We protect your personal data to avoid unintended loss or amendment, against unauthorized publication and against unauthorized persons accessing your personal data.   
+  We mainly use data processors in the EU who store and process personal data on behalf of Goodwings Experiences in accordance with this Privacy Policy and applicable legislation. However, information may be stored to a server that may be located inside or outside of the EU and the European Economic Area.
+Should a breach of security occur, we will inform you of this as soon as possible if the breach may result in high risk to your rights e.g. identity theft, economic loss, discrimination of you, loss of reputation or other significant inconvenience.</p>
+<h2>6.	USE OF COOKIES  
+</h2>
+<p>6.1	Cookies, purpose and relevance.</p>
+<p>We use cookies to collect information about you to adapt and improve our content on the website.
+If you do not want your data to be collected, please delete your cookies (see instructions below in section 6.2) and refrain from any further use of the website. We have elaborated on what data is being collected, the purpose and third-parties’ access to these below.</p>
+<p> 6.2	What are cookies?</p>
+<p>The website uses ”cookies” which is a text file that is saved on your computer, mobile phone or similar devices with the purpose of recognizing the device, remembering settings, performing statistics and targeting advertising. Cookies cannot contain damaging code such as viruses. 
+  It is possible to delete or block cookies. See instructions at: youronlinechoices.com/uk/faqs
+If you delete or block cookies, you can risk that the website will not operate optimally, as well as content not being accessible.</p>
+<p>6.3	Which cookies are used:
 </p>
-<h2>Regular handovers of the information</h2>
-<p>The information may be handed over for research purposes as described in the Terms of use that the user accepts before starting to use the service. The researchers may not publish any research results so that identifying information would be revealed or that any specific user could be detected.
-</p><p>
-Information considering users of a single Goodwings Experiences community may be handed over to the client who has founded that community or to the community administrators appointed by that client.
+<p>6.3.1	First-party cookies
 </p>
-<h2>Transfers of the information outside the EU and the European Economic Area</h2>
-<p>Information may be stored to a server that may be located inside or outside of the EU and the European Economic Area
+<p>[Insert first-party cookies] 
 </p>
-<h2>Register protection principles</h2>
-<p>The information is stored on computers. The access to the information is restricted with passwords and physical access to the computers is restricted by the server hosting company.
+<p>6.3.2	Third-party cookies 
+</p>
+<p>We allow third parties to place their cookies on our website (so-called third-party cookies). We use marketing and optimization cookies to optimize the services on our website. This includes Google Analytics which we use to analyze how you use our website. The collected data will be sent and stored at Google’s servers. 
+  Below you can read more about which cookies Goodwings Experiences uses as well as the purpose:
+</p>
+<p>Name:	Google Analyse/analytics.js<br>
+  Provider: 
+  Google Inc.<br>
+  Purpose:	Website analysis	<br>
+Expiration: 2 years</p>
+<h2>7.	YOUR RIGHTS AS DATA SUBJECT</h2>
+<p> 7.1	You have the right to access your personal data.</p>
+<p>At any given time, you have the right to receive information about the personal data we are processing about you, where the data is collected from, and what we are using it for. You also have the right to be informed about the period for which the personal data will be stored and who the recipients of your personal data are, if we transfer your personal data inside Denmark and/or abroad. 
+  If you request so, we can inform you of, and provide you with, a copy of the personal data we process concerning you. The access can, however, be limited to ensure other people’s privacy protection, trade secrets and intellectual property rights. 
+You can use your rights by contacting us. You will find our contact information at section 2.4.</p>
+<p>7.2	You have the right to have inaccurate personal data corrected, restricted or deleted. 
+</p>
+<p>If you think the personal data we are processing about you is inaccurate, you have the right to request correction or restriction. You must contact us and inform us about the inaccuracies and how to rectify the information or which information should be restricted.  
+  In some instances, we are obliged to delete your personal data. This might be the case if you withdraw your consent. If you are of the opinion that your personal data is no longer necessary in accordance to the purpose for which we collected it, you are entitled to request that the data is deleted. You can also contact us if you are of the opinion that your personal data is being processed contrary to applicable law or other legal obligations.    
+  We will investigate if the conditions are fulfilled when you make a request of having your personal data rectified, restricted or deleted. If so, we will carry out the amendments or deletion as fast as possible.  
+</p>
+<p>7.3	You have the right to object.</p>
+<p>You have the right to object, on grounds relating to your particular situation, to processing of personal data concerning you. You may also object to our transfer of your data for direct marketing purposes. You can use your rights by contacting us. You will find our contact information at the top. If your objection is justified, we will cease to process your personal data.</p>
+<p>7.4	You have the right to data portability.</p>
+<p>You may have the right to data portability to the extent that the processing is carried out by automated means. Data portability involves a right to receive the personal data concerning you, which you have provided to us, in a structured, commonly used and machine-readable format and you have the right to transmit the data to another controller without hindrance.</p>
+<p>7.5	File a complaint.</p>
+<p>If you wish to file a complaint about our processing of your personal data, you have the right to contact the Danish Data Protection Authority (“Datatilsynet”): https://www.datatilsynet.dk/media/6930/klage-til-datatilsynet.pdf
+  
+  
+  
+  
+  
 </p>
     </div>
   );
