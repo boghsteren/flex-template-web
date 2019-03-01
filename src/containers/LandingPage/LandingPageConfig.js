323737f55b48057ac86f660087edc10fb43352fd
Tri Nguyen <black.thanhtri@gmail.com>
2019-03-01 18:50:54 +0700

    tr - update listing id

--- src/containers/LandingPage/LandingPageConfig.js
+++ src/containers/LandingPage/LandingPageConfig.js
@@ -1,5 +1,5 @@
 export const promotedExperiences = [
-  "5be996ec-d20d-4691-bf0c-3a9c5cb0ffb8",
-  "5be994fc-e389-483b-b662-66945682c19f",
-  '5be99174-888a-4a13-a9c0-d0aee0b4c86b'
+  "5be95940-02e9-4a91-9700-108f5f33f12f",
+  "5bcd7df8-d511-4281-8cb8-1207b4631ec7",
+  "5bb201c2-9e5f-475f-ac16-8c369b182de9"
 ];
